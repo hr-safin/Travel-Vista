caa46d0c7157ea02800b1b50815b186e817c04fa
safinhr <hrsafin2434@gmail.com>
2024-01-03 23:56:56 +0600

    firebase added

---
+++ my-project/src/Firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAcgNx-sJKwtmfiho1JiCDbW14FxY1SFR8",
+  authDomain: "travel-vista-9c311.firebaseapp.com",
+  projectId: "travel-vista-9c311",
+  storageBucket: "travel-vista-9c311.appspot.com",
+  messagingSenderId: "816120150315",
+  appId: "1:816120150315:web:658d03f14eab9193c76d6e"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);

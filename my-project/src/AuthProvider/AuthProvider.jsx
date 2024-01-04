c73b5c52cff39c80b47bbc6e360b4127c9b1a4cb
safinhr <hrsafin2434@gmail.com>
2024-01-04 19:08:14 +0600

    setup authcontext

--- my-project/src/AuthProvider/AuthProvider.jsx
+++ my-project/src/AuthProvider/AuthProvider.jsx
@@ -1,17 +1,17 @@
 
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import { app } from "../Firebase/firebase.config";
 import { getAuth } from "firebase/auth";
 const AuthContext = createContext(null);
 export const AuthProvider = ({ children }) => {
 
   const auth = getAuth(app)
+  const [user, setUser] = useState(null)
 
    
 
-    const userInfo = {
-        name : "Hasibur rahman safin"
-    }
+
+    
   return <AuthContext.Provider value={userInfo}>
     {children}
     

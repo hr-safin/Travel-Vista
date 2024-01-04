e0057c8b12022a58be8a215fd84235acedb52918
safinhr <hrsafin2434@gmail.com>
2024-01-04 19:12:41 +0600

    passed authconext to chiildren

--- my-project/src/AuthProvider/AuthProvider.jsx
+++ my-project/src/AuthProvider/AuthProvider.jsx
@@ -11,7 +11,12 @@ export const AuthProvider = ({ children }) => {
    
 
 
-    
+    const userInfo = {
+      user,
+      setUser,
+      isLoading,
+      setIsLoading
+    }
   return <AuthContext.Provider value={userInfo}>
     {children}
     
